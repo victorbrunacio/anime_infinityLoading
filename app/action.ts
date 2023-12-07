c4f6fa47d7a1477217211a1bf4ba231895366a11
Victor Vinhaes <victorworkdeveloper@gmail.com>
2023-12-07 11:22:52 -0300

    up projetyc

---
+++ app/action.ts
@@ -0,0 +1,10 @@
+"use server"
+
+export const fetchAnime = async(page:number)=>{
+
+    const res = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`)
+    const data = await res.json()
+
+    return data
+    
+}
--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MotionDiv } from "./MotionDiv";
 
 export interface AnimeProp {
   id: string;
@@ -17,12 +18,30 @@ interface Prop {
   index: number;
 }
 
+const variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  }
+}
 function AnimeCard({ anime }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+    variants={variants}
+    initial="hidden"
+    animate="visible"
+    transition={{
+      delay:0.2,
+      ease:"easeInOut",
+      duration:0.5
+    }}
+    viewport={{amount:0.}}
+    className="max-w-sm rounded relative w-full">
       <div className="relative w-full h-[37vh]">
         <Image
-          src={anime.image.original}
+          src={`https://shikimori.one/${anime.image.original}`}
           alt={anime.name}
           fill
           className="rounded-xl"
@@ -64,7 +83,7 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
